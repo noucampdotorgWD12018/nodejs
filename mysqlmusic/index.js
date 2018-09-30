0ba1c0f25fffdce77c198d73568ae624c2751558
Thomas Devine <tomo@thomass-imac.home>
2018-09-30 21:51:20 +0100

    init

---
+++ mysqlmusic/index.js
@@ -0,0 +1,20 @@
+// index.js
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'music'
+});
+
+connection.connect();
+var query="SELECT * FROM artist";
+connection.query(query, function(err, rows, fields) {
+  if (err) throw err;
+  console.log(JSON.stringify(rows));
+  // OR
+  for(var i=0;i<rows.length;i++)
+    console.log(rows[i].artist_name);
+});
+connection.end();
